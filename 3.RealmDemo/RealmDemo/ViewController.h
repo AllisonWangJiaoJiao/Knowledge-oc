7eb443cad64a3b38a01d60e788f5e5c502424d73
wangjiaojiao <jiaojiao.wang@w-oasis.com>
2017-08-20 15:24:34 +0800

    realm init

---
+++ 3.RealmDemo/RealmDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealmDemo
+//
+//  Created by Allison on 2017/8/20.
+//  Copyright © 2017年 Allison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3.RealmDemo/RealmDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RealmDemo
+//
+//  Created by Allison on 2017/8/20.
+//  Copyright © 2017年 Allison. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
